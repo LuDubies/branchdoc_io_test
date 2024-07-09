bfc3e7dadd584775431548f6eac06fd973019694
Gabryel Reyes <gabryel.reyes@newtec.de>
2024-07-09 11:13:37 +0200

    Updated configuration and extensions

--- doc/conf.py
+++ doc/conf.py
@@ -9,13 +9,13 @@
 project = 'Branchdoc'
 copyright = '2024, Luca Dubies'
 author = 'Luca Dubies'
-release = '0.1'
+release = '1.0.0'
 conf_py_path = "/doc/" # with leading and trailing slashes
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['sphinxcontrib.mermaid','myst_parser', "sphinx_rtd_dark_mode"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -25,5 +25,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# HTML theme and static files
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_css_files = ['css/svg.css']
+
+# HTML theme options
+default_dark_mode = True
+
+# Mermaid configuration
+mermaid_output_format = 'svg'
+mermaid_cmd = 'mmdc'
+mermaid_cmd_shell = "true"
+mermaid_params = ['--theme', 'dark', '--backgroundColor', 'transparent']
